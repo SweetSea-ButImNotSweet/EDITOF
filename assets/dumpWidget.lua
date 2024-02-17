b3c951e6ed059b83baf879fff3e7b2b6bbda7b8f
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2024-02-17 18:53:17 +0700

    Adjusting the expected code's format of dumpWidget

--- assets/dumpWidget.lua
+++ assets/dumpWidget.lua
@@ -1,43 +1,44 @@
 local string_format=string.format
 
-local general_handling
-local function dump(key,val,tab)
+local step2
+local function step1(key,val,tab)
     local tab_ident=''
     if not tab then tab=1 end
     for _=1,tab do tab_ident=tab_ident..'\t' end  -- general_handling
 
-    key=general_handling(key,tab_ident,tab)
+    key=step2(key,tab_ident,tab,true)
     
     if (key:lower()):find('color',1,true) then
         val=string_format('COLOR.hex\'%s\'',COLOR.rgb2hex(unpack(val)))
     else
-        val=general_handling(val,tab_ident,tab)
+        val=step2(val,tab_ident,tab)
     end
 
 
-    return string_format('%s[%s]=%s',tab_ident,key,val)
+    return string_format('%s%s=%s',tab_ident,key,val)
 end
 
-general_handling=function(s,tab_ident,tab)
+step2=function(s,tab_ident,tab,isKey)
     local type_s=type(s)
-    local result
 
-    if type_s=='number' then
-        result=s
-    elseif type_s=='boolean' then
-        result=s and 'true' or 'false'
+    if type_s=='boolean' then
+        return s and 'true' or 'false'
     elseif type_s=='nil' then
-        result='nil'
+        return 'nil'
     elseif type_s=='table' then
         local temp='{\n'
         for k,v in pairs(s) do
-            temp=temp..dump(k,v,tab+1)..',\n'
+            temp=temp..step1(k,v,tab+1)..',\n'
         end
-        result=temp..tab_ident..'}'
+        return temp..tab_ident..'}'
+    elseif (        -- " --> \"
+        type_s=='string' and
+        string.find(s, '[^A-Za-z0-9_]')
+    ) then
+        return string_format(isKey and '[%q]' or '%q', s)
     else
-        result=string_format('%q',s) -- " --> \"
+        return tostring(s)
     end
-    return result
 end
 
 
@@ -61,10 +62,10 @@ return function (w,exportAs)
         return l
     else
         local result='{\n'
-        result=string_format('%s%s,\n',result,dump('type',w.type,1))
+        result=string_format('%s%s,\n',result,step1('type',w.type,1))
         for _,k in pairs(originalW.buildArgs) do
             if w[k]~=originalW[k] then
-                result=string_format('%s%s,\n',result,dump(k,w[k],1))
+                result=string_format('%s%s,\n',result,step1(k,w[k],1))
             end
         end
         result=result..'}'
