2edef34da0cde9e33fb2e3327007d981971f820b
SweetSea <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2024-02-20 12:53:54 +0700

    Update color.lua (ZenithaExtended)

--- ZenithaExtended/color.lua
+++ ZenithaExtended/color.lua
@@ -1,3 +1,4 @@
+local toHex=STRING.toHex
 -- local function toHex(n)     --- ONLY USED BY COLOR.rgb2hex
 --     local sub,floor=string.sub,math.floor
 --     local hexBase='0123456789ABCDEF'
@@ -21,17 +22,17 @@
 function COLOR.rgb2hex(r,g,b)
     assertf(
         (
-            type(r)=='number' and r>0 and r<=255 and
-            type(g)=='number' and g>0 and g<=255 and
-            type(b)=='number' and b>0 and b<=255
+            type(r)=='number' and r>=0 and r<=255 and
+            type(g)=='number' and g>=0 and g<=255 and
+            type(b)=='number' and b>=0 and b<=255
         ),
         'Require r, g, b need to be in 0-1 or 0-255, got r=%s, g=%s, b=%s',
         tostring(r), tostring(g), tostring(b)
     )
     local Rh,Gh,Bh
-    Rh=STRING.toHex((r<=1 and r*255) or r)
-    Gh=STRING.toHex((g<=1 and g*255) or g)
-    Bh=STRING.toHex((b<=1 and b*255) or b)
+    Rh=toHex((r%1~=0 and r*255) or r)
+    Gh=toHex((g%1~=0 and g*255) or g)
+    Bh=toHex((b%1~=0 and b*255) or b)
 
     return Rh..Gh..Bh
 end
