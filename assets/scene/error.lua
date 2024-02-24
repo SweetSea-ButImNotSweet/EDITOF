0a90351028c4aebbad84711ecde09a67d41a0e85
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2024-02-25 01:06:37 +0700

    Remake error screen

--- assets/scene/error.lua
+++ assets/scene/error.lua
@@ -1,5 +1,5 @@
 local BGcolor
-local sysAndScn,errorText
+local currentScn,errorText
 
 local scene={}
 
@@ -11,41 +11,45 @@ function scene.enter()
 
     err=Zenitha.getErr('#') or {
         scene="NULL",
-        msg={"??????????????????????????","","Traceforward","??????","?????","????","???","??","?"},
-        shot=GC.load{200,120,
-            {'setLW',2},
-            {'setCL',1,1,1,.2},
-            {'fRect',0,0,200,120},
-            {'setCL',COLOR.L},
-            {'setFT',60},
-            {'print','?',118,95,MATH.pi},
+        msg={
+            "KHOAN! TAO ĐÃ LÀM GÌ SAI??? MÀ MÀY TỚI TÌM TAO VẬY???\nAPP VẪN CÒN CHẠY, VẪN SỐNG NHĂN RĂNG KÌA\nCHẲNG QUA MÀY CỐ MỞ MÀN HÌNH NÀY THÔI!",
+            "",
+            "Bộ mấy ông không tin tưởng được hay sao mà phải mò tới đây để hỏi tao là app có bug hay không?",
+            "",
+            "Xin thưa, app nào trước giờ cũng chả có bug. App nào mà không gặp bug thì chỉ có app very đơn giản hoặc là ông dev đó ắt hẳn phải là thần thánh rồi.",
+            "",
+            "Còn bây giờ nếu app gặp bug thì cứ tạo issue trên GitHub đi!",
+            "(mà làm ơn đừng có tạo issue trùng với issue đã tạo rồi đấy!)",
+            "",
+            "Mày nhấn ESC được rồi đó",
+            "(hoặc nhấn nút 'QUIT' trên màn hình cũng được)"
         },
+        shot=IMG.error_placeholder
     }
-    BGcolor=math.random()>.026 and {.3,.5,.9} or {.62,.3,.926}
-    sysAndScn="Something4Fun        scene:"..err.scene
-    errorText="WTF my application crashed again?\nPlease tell me, SweetSea, about this error ASAP so I can fix."
+    BGcolor=Zenitha.getErr('#') and {.0,.0,.5} or {.1,.5,.9}
+    currentScn="scene:"..err.scene
+    errorText ="EDITOF has unexpectedly stopped working!\n#tấtcảlàtạiMagiaBaiser #tấtcảlàtạiUtenaHiigari"
 end
 
 function scene.draw()
     GC.clear(BGcolor)
     GC.setColor(1,1,1)
-    GC.draw(err.shot,100,326,nil,512/err.shot:getWidth(),288/err.shot:getHeight())
+    GC.draw(err.shot,100,230,nil,512/err.shot:getWidth(),288/err.shot:getHeight())
 
     GC.setColor(COLOR.LL)
-    FONT.set(100)GC.print(":(",100,0,0,1.2)
-    FONT.set(40)GC.printf(errorText,100,160,SCR.w0-100)
+    FONT.set(40,'monoBI')GC.printf(errorText,100,55,SCR.w0-100)
 
-    FONT.set(20,'_mono')
-    GC.print(sysAndScn,100,630)
+    FONT.set(20,'monoB')
+    GC.print(currentScn,100,200)
+    GC.printf(err.msg[1],626,200,600)
 
-    FONT.set(15,'_mono')
-    GC.printf(err.msg[1],626,326,650)
-    GC.printf(table.concat(err.msg,'\n',2),626,386,650)
+    FONT.set(17,'monoM')
+    GC.printf(table.concat(err.msg,'\n',2),626,300,600)
 end
 
 scene.widgetList={
-    WIDGET.new{type='button',name='console',x=940,y=85,w=170,h=80,fontSize=65,fontType='symbols',text=CHAR.icon.console,code=WIDGET.c_goScn'_console'},
-    WIDGET.new{type='button',name='quit',x=1140,y=85,w=170,h=80,fontSize=60,fontType='symbols',text=CHAR.icon.cross_big,code=WIDGET.c_pressKey'escape'},
+    WIDGET.new{type='button',name='console',x=225,y=570,w=245,h=80,cornerR=0,fontSize=40,fontType='symbols',text=CHAR.icon.console  ..' Terminal',code=WIDGET.c_goScn'_console'},
+    WIDGET.new{type='button',name='quit'   ,x=485,y=570,w=245,h=80,cornerR=0,fontSize=40,fontType='symbols',text=CHAR.icon.cross_big..' Quit',code=WIDGET.c_pressKey'escape'},
 }
 
 return scene
